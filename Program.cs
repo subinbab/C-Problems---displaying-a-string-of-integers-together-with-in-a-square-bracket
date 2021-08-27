d4e066525cf3f05530838e3a2912f3d4afb5b174
subinbab <subinbabu3127@gamil.com>
2021-08-27 15:31:00 +0530

    This code is for displaying a string of integers togeter with in a '[]'

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 10,23,45,67,22,47,89};
+            Console.WriteLine("[");
+            for(int i =0;i<a.Length-1;i++){
+                Console.Write(a[i]+",");
+            }
+
+            Console.Write(a[a.Length-1]+"]");
+        }
+    }
+}
